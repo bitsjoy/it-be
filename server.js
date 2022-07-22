1b89511efc22d92f2774755a4270b3034939fec4
Neelesh <nsharma1@me.iitr.ac.in>
2022-07-22 18:27:29 +0530

    A

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://bitsjoy.github.io", );
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
